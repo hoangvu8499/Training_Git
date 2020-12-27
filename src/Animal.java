a01caa23ce106b969dccb1eb7f93dc76ef741288
hoangvu8499 <hoangvu8499@gmail.com>
2020-12-27 11:28:44 +0700

    print_cat

--- src/Animal.java
+++ src/Animal.java
@@ -60,5 +60,7 @@ public class Animal {
         Animal dog = new Animal(2,50.2,"Dog",10.1);
         Animal cat = new Animal(1,50.2,"Cat",10.1);
         System.out.println(dog.toString());
+        System.out.println(cat.toString());
+
     }
 }
