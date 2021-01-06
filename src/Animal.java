d1cf9784cccc50406515754f713db0d1540c3f00
hoangvu8499 <hoangvu8499@gmail.com>
2021-01-06 15:28:18 +0700

    show pig

--- src/Animal.java
+++ src/Animal.java
@@ -57,12 +57,12 @@ public class Animal {
     }
 
     public static void main(String[] args) {
-        Animal pig = new Animal(2,50.2,"pig",10.1);
+        Animal pig = new Animal(2, 50.2, "pig", 10.1);
 
-        Animal cat = new Animal(1000,50.2,"Cat",10.1);
+        Animal cat = new Animal(1000, 50.2, "Cat", 10.1);
         System.out.println(cat.toString());
 
-        Animal dog = new Animal(2,50.2,"Dog",10.1);
+        Animal dog = new Animal(2, 50.2, "Dog", 10.1);
 
         System.out.println(pig.toString());
 
